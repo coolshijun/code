0cd78fb18688187f8c0af590d102d25a52bb2827
Simon Shi <coolshijun@hotmail.com>
2017-05-05 23:20:04 +0800

    Project created

---
+++ leet/src/main/java/com/code/leet/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.code.leet;
+
+/**
+ * @author simon
+ *
+ */
+public class Solution {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello world!");
+	}
+}
